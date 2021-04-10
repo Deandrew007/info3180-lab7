47721c98a1bbd1e59ca362702f70ace83bb4dd52
Deandrew007 <deamoore07@gmail.com>
2021-04-10 18:14:23 -0500

    build form

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms.validators import InputRequired
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+
+class UploadForm(FlaskForm):
+    description = StringField('Description', validators=[InputRequired()])
+    photo = FileField('Photo', validators=[DataRequired()])
 
